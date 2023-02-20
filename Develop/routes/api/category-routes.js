b6bec1ef9b9c668985430a0555d863e45ec1c796
Call Carl <callienicole0419@gmail.com>
2023-02-20 17:53:21 -0600

    updated post route in category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -15,7 +15,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  
+  Category.create({
+    //insert info here
+  })
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
