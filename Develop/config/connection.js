20e29f7eeb8a28495332518a209325b585a21d2e
Call Carl <callienicole0419@gmail.com>
2023-02-20 13:07:55 -0600

    updated server.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,12 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-    //working with hardoded info but not process.env
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PW, 
-    {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
