9a794352716b9bf0f2b4d37ee194ca53f7d5807e
Call Carl <callienicole0419@gmail.com>
2023-02-20 18:01:30 -0600

    updated get routes in category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,11 +5,18 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
   // be sure to include its associated Products
+
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findByPk(req.params.id).then((categoryData) => {
+    res.json(categoryData);
+  });
   // be sure to include its associated Products
 });
 
