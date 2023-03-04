75a6f9fb1b90e6d5569d3bb4e6f9066b84526006
Call Carl <callienicole0419@gmail.com>
2023-03-03 18:09:43 -0600

    added models to server.js, added product belongs to category in index.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,11 +4,9 @@ const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection.js');
 
-//import models to sync table with database// initializing
-//const Category = require('./models/Category.js');
-//const Product = require('./models/Product.js');
-//const ProductTag = require('./models/ProductTag.js');
-//const Tag = require('./models/Tag.js');
+//import models to sync with database
+const models = require('./models');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
